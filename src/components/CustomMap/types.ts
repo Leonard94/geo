63bae2a797304c2ab938be3cd7fbcba305d98fb4
Владислав Коледа <vladislav.koleda94@gmail.com>
2024-09-03 12:54:39 +0300

    feat

--- src/components/CustomMap/types.ts
+++ src/components/CustomMap/types.ts
@@ -3,10 +3,32 @@ export interface IPolygon {
   coordinates: number[][]
 }
 
+// export type IPoint = any
+// export interface IPoint {
+//   id: string
+//   lat: number
+//   lon: number
+//   title: string
+//   address: string
+//   comment: string
+//   validity: boolean
+//   objectType: EObjectType
+// }
 export interface IPoint {
+  type: 'Feature'
   id: string
-  lat: number
-  lon: number
+  geometry: {
+    type: 'Point'
+    coordinates: [number, number]
+  }
+  properties: {
+    balloonContent?: string
+    clusterCaption?: string
+    hintContent?: string
+    balloonContentHeader?: string
+    balloonContentBody?: string
+    balloonContentFooter?: string
+  }
   title: string
   address: string
   comment: string
@@ -17,7 +39,7 @@ export interface IPoint {
 export enum EObjectType {
   BPLA = 'БПЛА',
   ROCKET = 'Обстрел',
-  ObPAO = 'ОбПАО'
+  ObPAO = 'ОбПАО',
 }
 
 export interface IPlaceMarkPointProps {
--- src/components/ExcelUploader/ExcelUploader.tsx
+++ src/components/ExcelUploader/ExcelUploader.tsx
@@ -1,21 +1,43 @@
 import React, { useState } from 'react'
 import * as XLSX from 'exceljs'
-import { EObjectType } from '../CustomMap/types'
+import { EObjectType, IPoint } from '../CustomMap/types'
 import { Button } from '@mui/material'
 
-interface Point {
-  id: string
-  title: string
-  address: string
-  comment: string
-  validity: boolean
-  objectType: EObjectType
-  lat: number
-  lon: number
-}
 
 interface ExcelUploaderProps {
-  onPointsUpdate: (points: Point[]) => void
+  onPointsUpdate: (points: IPoint[]) => void
+}
+
+const createBalloonContent = (point: IPoint) => {
+  const content = []
+  const { title, address, comment, objectType, validity } = point
+
+  if (title) {
+    content.push(`<b>${title}</b>`)
+  }
+  if (address) {
+    content.push(`<p>Адрес: ${address}</p>`)
+  }
+  if (comment) {
+    content.push(`<p>Комментарий: ${comment}</p>`)
+  }
+  if (objectType) {
+    content.push(`<p>Тип объекта: ${objectType}</p>`)
+  }
+  if (validity !== undefined) {
+    content.push(
+      `<p>${validity ? 'Информация актуальна' : 'Информация не актуальна'}</p>`
+    )
+  }
+
+  // content.push(`
+  //   <div class="row">
+  //     <button id="editButton_${id}" class="btn btn_edit">Редактировать</button>
+  //     <button id="deleteButton_${id}" class="btn btn_remove">Удалить</button>
+  //   </div>
+  // `)
+
+  return `<div>${content.join('')}</div>`
 }
 
 export const ExcelUploader: React.FC<ExcelUploaderProps> = ({
@@ -48,57 +70,37 @@ export const ExcelUploader: React.FC<ExcelUploaderProps> = ({
         return
       }
 
-      console.time('handleFileUpload')
-
-      const newPoints: Point[] = []
+      const newPoints: IPoint[] = []
 
       worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
         if (rowNumber === 1) {
           return
         }
 
-        const point: Point = {
+        const point: IPoint = {
           id: row.getCell(1).value?.toString() || '',
           title: row.getCell(2).value?.toString() || '',
           address: row.getCell(3).value?.toString() || '',
           comment: row.getCell(4).value?.toString() || '',
           validity: ['да', 'Да', 'ДА'].includes(String(row.getCell(5).value)),
           objectType: row.getCell(6).value?.toString() as EObjectType,
-          lat: Number(row.getCell(7).value) || 0,
-          lon: Number(row.getCell(8).value) || 0,
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [
+              Number(row.getCell(7).value) || 0,
+              Number(row.getCell(8).value) || 0,
+            ],
+          },
+          properties: {
+            balloonContent: 'Temporarily unavailable',
+            clusterCaption: 'Temporarily unavailable',
+          },
         }
+        point.properties.balloonContent = createBalloonContent(point)
+        point.properties.clusterCaption = point.title
 
         newPoints.push(point)
-      })
-
-      newPoints.sort((a, b) => {
-        if (a.lon !== b.lon) {
-          return a.lon - b.lon
-        } else {
-          return a.lat - b.lat
-        }
-      })
-
-      newPoints.forEach((point, index) => {
-        if (!newPoints[index + 1]) {
-          return
-        }
-
-        if (point.lon !== newPoints[index + 1].lon) {
-          return
-        }
-
-        if (point.lat !== newPoints[index + 1].lat) {
-          return
-        }
-
-        const newTitle = point.title + ', ' + newPoints[index + 1].title
-        const newId = point.id + ', ' + newPoints[index + 1].id
-
-        newPoints[index + 1].title = newTitle
-        newPoints[index + 1].id = newId
-
-        return null
       })
 
       onPointsUpdate(newPoints)
